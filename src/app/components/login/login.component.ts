255fe37e7bccbff051e4a0e49f0b8bb536b0ad64
Rawennee <silvarawenne@gmail.com>
2025-11-28 16:19:02 -0300

    fix: Restaura logica de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Credenciais } from 'src/app/models/Credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  // MUDANÇA 1: Usamos templateUrl para apontar para o arquivo HTML
   templateUrl: './login.component.html',
-  // MUDANÇA 2: Usamos styleUrls para apontar para o arquivo CSS
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logar() {
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.successfulLogin(resposta.headers.get('Authorization').substring(7));
+      this.router.navigate(['home'])
+    }, () => {
+      this.toast.error('Usuário e/ou senha inválidos');
+    })
   }
 
 }
